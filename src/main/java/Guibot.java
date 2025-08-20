df7f787bad8ee927cd25df73308b901fcacc6297
Estelle <e1354618@u.nus.edu>
2025-08-21 01:30:07 +0800

    Level 1 - Echo
    Chatbot echoes commands entered by the user and exits when the user
    types the command bye

--- src/main/java/Guibot.java
+++ src/main/java/Guibot.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Guibot {
     public static void main(String[] args) {
+	Scanner scanner = new Scanner(System.in);
 	String line = "____________________________________________________________";
-        System.out.println(line + "\nHello! I'm Guibot\nWhat can I do for you?\n" + line + "\nBye. Hope to see you again soon!\n" + line);
+
+        System.out.println("\t" + line + "\n\t  Hello! I'm Guibot\n\t  What can I do for you?\n\t" + line + "\n");
+
+	String input = scanner.nextLine();
+
+	while(!input.equals("bye")) {
+		System.out.println("\n\t" + line + "\n\t  " + input + "\n\t" + line + "\n");
+		input = scanner.nextLine();
+	}
+
+	System.out.println("\n\t" + line + "\n\t  Bye. Hope to see you again soon!\n\t" + line + "\n");
     }
 }

7153ecb73ecb960cecf6a183cd5997d7cfc2301b
Estelle <e1354618@u.nus.edu>
2025-08-21 03:45:49 +0800

    Level 3 - Mark as Done
    Mark tasks as done, change them back to undone.

--- src/main/java/Guibot.java
+++ src/main/java/Guibot.java
@@ -4,23 +4,45 @@ import java.util.ArrayList;
 public class Guibot {
     public static void main(String[] args) {
 	Scanner scanner = new Scanner(System.in);
-	String line = "____________________________________________________________";
+	String line = "_________________________________________________________________________________________________";
 
         System.out.println("\t" + line + "\n\t  Hello! I'm Guibot\n\t  What can I do for you?\n\t" + line + "\n");
 
 	String input = scanner.nextLine();
-	ArrayList<String> list = new ArrayList<>();
+	TaskList tasklist = new TaskList();
 
 	while(!input.equals("bye")) {
+
 		System.out.println("\n\t" + line);
 
-		if (input.equals("list")) {
-			for (int i = 0; i < list.size(); i++) {
-				System.out.println(String.format("\n\t  %d. %s", i + 1, list.get(i)));
+		try {
+
+			if (input.equals("list")) {
+
+				System.out.println("\n\t  Here are the tasks in your list:" + tasklist.toString());
+
+			} else if (input.matches("mark [1-9][0-9]*")) {
+
+				int taskNumber = Integer.parseInt(input.replaceAll("[^0-9]", ""));
+			
+				System.out.println("\n\t  Nice! I've marked this task as done:\n\t  " + tasklist.mark(taskNumber - 1));
+
+			} else if (input.matches("unmark [1-9][0-9]*")) {
+
+				int taskNumber = Integer.parseInt(input.replaceAll("[^0-9]", ""));
+			
+				System.out.println("\n\t  OK, I've marked this task as not done yet:\n\t  " + tasklist.unmark(taskNumber - 1));
+
+			} else {
+
+				System.out.println("\n\t  Ok, I've added: \n\t  " + tasklist.add(input));
+
 			}
-		} else {
-			list.add(input);
-			System.out.println("\n\t  added: " + input);
+
+		} catch (TaskNotFoundException e) {
+
+			System.out.println("\n\t  The index specified does not correspond to a task in the list. Please try again");
+
 		}
 
 		System.out.println("\n\t" + line + "\n");
---
+++ src/main/java/TaskList.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class TaskList {
+	private ArrayList<Task> tasks;
+
+	public TaskList() {
+		this.tasks = new ArrayList<>();
+	}
+
+	public String add(String s) {
+		Task t = new Task(s);
+		this.tasks.add(t);
+		return t.toString();
+	}
+
+	public String mark(int index) throws TaskNotFoundException {
+		if (index >= 0 && index < this.tasks.size()) {
+			this.tasks.get(index).mark();
+			return this.tasks.get(index).toString();
+		} else {
+			throw new TaskNotFoundException();
+		}
+	}
+
+	public String unmark(int index) throws TaskNotFoundException {
+		if (index >= 0 && index < this.tasks.size()) {
+			this.tasks.get(index).unmark();
+			return this.tasks.get(index).toString();
+		} else {
+			throw new TaskNotFoundException();
+		}
+	}
+
+	public String toString() {
+		String s = "";
+
+		for (int i = 0; i < this.tasks.size(); i++) {
+			s += String.format("\n\t  %d.%s", i + 1, this.tasks.get(i).toString());
+		}
+
+		return s;
+	}
+}
+
+	

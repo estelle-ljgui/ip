f54ca16616257fdc3ca410c49f8d062157aba62e
Estelle <e1354618@u.nus.edu>
2025-08-21 00:59:40 +0800

    Level 0 - Rename, Greet, Exit
    Renamed from Duke to Guibot and changed all references to Duke in source
    code. Chatbot will greet then exit.

---
+++ src/main/java/Guibot.java
@@ -0,0 +1,6 @@
+public class Guibot {
+    public static void main(String[] args) {
+	String line = "____________________________________________________________";
+        System.out.println(line + "\nHello! I'm Guibot\nWhat can I do for you?\n" + line + "\nBye. Hope to see you again soon!\n" + line);
+    }
+}

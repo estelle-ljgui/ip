4f9ea60af1e89fa48b3b3d70a30b94cd85f2cc35
Estelle <e1354618@u.nus.edu>
2025-08-21 01:51:19 +0800

    Level 2 - Add, List
    Stores whatever text entered by the user and displays text when
    requested

--- src/main/java/Guibot.java
+++ src/main/java/Guibot.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Guibot {
     public static void main(String[] args) {
@@ -8,9 +9,21 @@ public class Guibot {
         System.out.println("\t" + line + "\n\t  Hello! I'm Guibot\n\t  What can I do for you?\n\t" + line + "\n");
 
 	String input = scanner.nextLine();
+	ArrayList<String> list = new ArrayList<>();
 
 	while(!input.equals("bye")) {
-		System.out.println("\n\t" + line + "\n\t  " + input + "\n\t" + line + "\n");
+		System.out.println("\n\t" + line);
+
+		if (input.equals("list")) {
+			for (int i = 0; i < list.size(); i++) {
+				System.out.println(String.format("\n\t  %d. %s", i + 1, list.get(i)));
+			}
+		} else {
+			list.add(input);
+			System.out.println("\n\t  added: " + input);
+		}
+
+		System.out.println("\n\t" + line + "\n");
 		input = scanner.nextLine();
 	}
 

b63960aa5970c262191b517f2898a1acca4aba66
Estelle <e1354618@u.nus.edu>
2025-09-01 23:54:46 +0800

    Use date and time for deadlines and events
    Use java.time.LocalDateTime for the deadline of deadlines and the start and end of
    ebents

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,17 +1,21 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-	private String deadline;
+	private LocalDateTime deadline;
+	private final static DateTimeFormatter DATE_TIME_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
 
 	public Deadline(String[] arguments) {
 		super(arguments[0]);
-		this.deadline = arguments[1];
+		this.deadline = LocalDateTime.parse(arguments[1], DATE_TIME_FORMAT);
 	}
 
 	public String toStorageString() {
-		return "d//" + super.toStorageString() + "/" + this.deadline;
+		return "d//" + super.toStorageString() + "/" + this.deadline.format(DATE_TIME_FORMAT);
 	}
 
 	@Override
 	public String toString() {
-		return "[D]" + super.toString() + " (by: " + this.deadline + ")";
+		return "[D]" + super.toString() + " (by: " + this.deadline.format(DateTimeFormatter.ofPattern("MMM d yyyy h.mma")) + ")";
 	}
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,19 +1,27 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-	private String from;
-	private String to;
+	private LocalDateTime from;
+	private LocalDateTime to;
+	private final static DateTimeFormatter DATE_TIME_FORMAT = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
 
 	public Event(String[] arguments) {
 		super(arguments[0]);
-		this.from = arguments[1];
-		this.to = arguments[2];
+		this.from = LocalDateTime.parse(arguments[1], DATE_TIME_FORMAT);
+		this.to = LocalDateTime.parse(arguments[2], DATE_TIME_FORMAT);
 	}
 
 	public String toStorageString() {
-		return "e//" + super.toStorageString() + "/" + this.from + "/" + this.to;
+		return "e//" + super.toStorageString() + "/" +
+			this.from.format(DATE_TIME_FORMAT) + "/" +
+			this.to.format(DATE_TIME_FORMAT);
 	}
 
 	@Override
 	public String toString() {
-		return "[E]" + super.toString() + " (from: " + this.from + " to: " + this.to + ")";
+		return "[E]" + super.toString() +
+			" (from: " + this.from.format(DateTimeFormatter.ofPattern("MMM d yyyy h.mma")) +
+			" to: " + this.to.format(DateTimeFormatter.ofPattern("MMM d yyyy h.mma")) + ")";
 	}
 }
